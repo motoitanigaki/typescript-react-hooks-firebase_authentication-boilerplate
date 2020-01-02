128aed4fedea65101e074cbc2be367b9807bcf4d
谷垣基 <motoitanigaki@tanigakihajimenoMacBook-Pro.local>
2020-01-02 17:54:01 +0900

    mask firebase authentication config

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/firebase-firestore";
 import firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyCGuXTL1-ngumUAB3WmRdykzOMiW98ccjs",
-  authDomain: "s2m-test.firebaseapp.com",
-  databaseURL: "https://s2m-test.firebaseio.com",
-  projectId: "s2m-test",
-  storageBucket: "s2m-test.appspot.com",
-  messagingSenderId: "749987469339",
-  appId: "1:749987469339:web:4d0a9f298d0ac70bf500be"
+  apiKey: "XXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXX"
 };
 firebase.initializeApp(config);
 const auth = firebase.auth();
